b5be0665f0defa7fca18de816b47507b69f3948a
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2021-05-11 13:37:43 +0630

    Functional Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,59 +1,55 @@
-import React from "react";
 import { Card, CardImg, CardBody, CardTitle, CardText, List } from "reactstrap";
 
-class DishDetail extends React.Component {
-  renderDish(dish) {
-    return (
-      <div className="col-12 col-md-5 col-xs-12 m-1">
-        <Card>
-          <CardImg top width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle tag="h5">{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      </div>
-    );
-  }
+function RenderDish({ dish }) {
+  return (
+    <div className="col-12 col-md-5 col-xs-12 m-1">
+      <Card>
+        <CardImg top width="100%" src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle tag="h5">{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
 
-  renderComments(comments) {
-    if (comments != null && comments.length > 0) {
-      return comments.map((cmt) => {
-        return (
-          <li key={cmt.id} className="mt-4">
-            <div>{cmt.comment}</div>
-            <code>
-              --{cmt.author},
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              }).format(new Date(cmt.date))}
-            </code>
-          </li>
-        );
-      });
-    } else {
-      return <li></li>;
-    }
-  }
-
-  render() {
-    if (this.props.dish != null) {
+function RenderComments({ comments }) {
+  if (comments != null && comments.length > 0) {
+    return comments.map((cmt) => {
       return (
-        <div className="row">
-          {this.renderDish(this.props.dish)}
-
-          <List type="unstyled" className="col-md-5 col-xs-12 m-1">
-            <h4>Comments</h4>
-            {this.renderComments(this.props.dish.comments)}
-          </List>
-        </div>
+        <li key={cmt.id} className="mt-4">
+          <div>{cmt.comment}</div>
+          <code>
+            --{cmt.author},
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            }).format(new Date(cmt.date))}
+          </code>
+        </li>
       );
-    } else {
-      return <div></div>;
-    }
+    });
+  } else {
+    return <li></li>;
   }
 }
+
+const DishDetail = (props) => {
+  if (props.dish != null) {
+    return (
+      <div className="row">
+        <RenderDish dish={props.dish} />
+        <List type="unstyled" className="col-md-5 col-xs-12 m-1">
+          <h4>Comments</h4>
+          <RenderComments comments={props.dish.comments} />
+        </List>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+};
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Card,
   CardImg,
@@ -8,31 +7,30 @@ import {
   Button,
 } from "reactstrap";
 
-class Menu extends React.Component {
-  render() {
-    const menu = this.props.dishes.map((dish) => {
-      return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={()=>this.props.onClick(dish.id)}>
-            <CardImg
-              top
-              width="100%"
-              src={dish.image}
-              alt={dish.name}
-            />
-            <CardBody>
-              <CardTitle tag="h5">{dish.name}</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">
-                {dish.label}
-              </CardSubtitle>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-        </div>
-      );
-    });
-    return menu
-  }
+function RenderMenuItem({ dish, onClick }) {
+  return (
+    <Card onClick={() => onClick(dish.id)}>
+      <CardImg top width="100%" src={dish.image} alt={dish.name} />
+      <CardBody>
+        <CardTitle tag="h5">{dish.name}</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">
+          {dish.label}
+        </CardSubtitle>
+        <Button>Button</Button>
+      </CardBody>
+    </Card>
+  );
 }
+
+const Menu = (props) => {
+  const menu = props.dishes.map((dish) => {
+    return (
+      <div key={dish.id} className="col-12 col-md-5 m-1">
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
+      </div>
+    );
+  });
+  return menu;
+};
 
 export default Menu;
